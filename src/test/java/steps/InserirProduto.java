c9a93de20f111ff36356b4bd1e33ff51c1e7df7b
Leo <conceicao.leonardo9@gmail.com>
2022-12-28 13:53:05 -0300

    pesquisa funcionando

--- src/test/java/steps/InserirProduto.java
+++ src/test/java/steps/InserirProduto.java
@@ -16,30 +16,29 @@ public class InserirProduto {
     public void queEstouAcessandoOSite() throws Throwable {
         System.setProperty("webdriver.chrome.driver", "/home/familia/Drivers/chromedriver_linux64/chromedriver");
         driver = new ChromeDriver();
-        driver.get("https://www.amazon.com.br/");
+        driver.get("https://www.amazon.com.br/");// entrando no site da amazon
     }
 
     @Entao("^visualizo a pagina inicial$")
     public void visualizoAPaginaInicial() throws Throwable {
         String texto = driver.findElement(By.xpath("//span[text()='© 2021-2022 Amazon.com, Inc. ou suas afiliadas']")).getText();
-        Assert.assertEquals("© 2021-2022 Amazon.com, Inc. ou suas afiliadas", texto);
+        Assert.assertEquals("© 2021-2022 Amazon.com, Inc. ou suas afiliadas", texto);// verificando se estou na pagina da amazon
 
     }
 
     @Entao("^pesquiso por um produto$")
     public void pesquisoPorUmProduto() throws Throwable {
-
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("fone de ouvido");// fazendo a pesquisa de um produto
     }
 
     @Entao("^aperto no botao de pesquisar$")
     public void apertoNoBotaoDePesquisar() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        driver.findElement(By.id("nav-search-submit-button")).click();// click na lupa de pesquisa
+        System.out.println("pesquisa feita");// confirmando no console esse passo foi feito
     }
 
     @Entao("^visualizo os resultados da pesquisa$")
     public void visualizoOsResultadosDaPesquisa() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
